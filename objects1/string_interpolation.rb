9ba977d08d7ed4347cc199986abeb1b80f79542f
IsCathal <mccabemcathal@gmail.com>
2024-04-26 17:43:16 +0100

    string interpolation

---
+++ objects1/string_interpolation.rb
@@ -0,0 +1,17 @@
+name = "charlie"
+
+puts "Hello #{name}, how are you?"
+
+age = 30
+
+#can do
+# puts "I am + age + "years old"
+#can add ruby code in brackets
+puts " I am #{age + 1} years old"
+
+x = 8
+
+y = 9
+
+#ruby will run code in brackets first
+puts "The sum of #{x} and #{y} is #{x + y}"
