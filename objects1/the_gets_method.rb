9812f71a6606fba06f82b689e120b054c6329650
IsCathal <mccabemcathal@gmail.com>
2024-04-26 18:17:17 +0100

    get

---
+++ objects1/the_gets_method.rb
@@ -0,0 +1,12 @@
+puts "Hi what's your name?"
+
+name = gets.chomp
+#ruby uses enter key as a line break
+#can add nethods to metods
+puts " Your name is #{name} ? Cool"
+
+puts " Great, whats your age?"
+p age = 345
+age = gets.chomp
+
+puts "Your name is #{name} and you are #{age} years old"
