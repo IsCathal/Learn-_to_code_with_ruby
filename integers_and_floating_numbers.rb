c331a70ef4204fe610515e6a736e295b81b991e6
IsCathal <mccabemcathal@gmail.com>
2024-04-26 08:06:43 +0100

    ints and floats

---
+++ integers_and_floating_numbers.rb
@@ -0,0 +1,12 @@
+puts 100
+puts 0
+puts -234
+puts 1_000
+
+
+puts
+puts
+puts 3.14151
+puts 0.40
+#cant to do puts puts .50
+
--- the_p_method.rb
+++ the_p_method.rb
@@ -17,3 +17,10 @@ p "LineBreak\n Test"
 
 # puts "test"
 # commmand plus forward slash is how to comment whole line
+
+=begin
+  big old comment section\hwre
+
+  look at my coments
+  
+end
