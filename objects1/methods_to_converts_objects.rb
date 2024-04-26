0e710bd9234aee1cc5c08d6e784da06a5877b597
IsCathal <mccabemcathal@gmail.com>
2024-04-26 20:00:55 +0100

    objects

---
+++ objects1/methods_to_converts_objects.rb
@@ -0,0 +1,29 @@
+text = "5"
+
+puts text 
+puts text.class
+puts text.to_i
+puts text.to_i.class
+
+puts "15 apples".to_i
+#really onlly works at the start
+
+puts "apples 15".to_i
+
+
+puts text.to_f
+puts text.to_f.class
+
+number = 5
+puts number.class
+puts number.to_s.class
+
+puts
+
+#think could be a user input would be noce to know its a string
+#polymorisism
+puts "5".to_s
+puts "5".to_s.class
+
+puts 4.to_i
+#int suports to int, to sting, to float etc
---
+++ objects1/the_class_method.rb
@@ -0,0 +1,9 @@
+#Class - A blueprint for an object(template/schematic)
+#Object - An instance of the class
+
+puts "hello".class
+
+puts "Ruby is fun".class
+
+puts
+puts 5.class
