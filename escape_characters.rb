f982058512f2444614aa934a04650d26ed43a1eb
IsCathal <mccabemcathal@gmail.com>
2024-04-26 07:22:17 +0100

    escape cha

---
+++ escape_characters.rb
@@ -0,0 +1,11 @@
+puts "some text\n more text"
+#escape charachter
+puts "\tOnbe upon a time"
+#t is a tab
+
+puts "Juiliet said \"Goodbye\" to romeo"
+#escapeing qoutes to add a quote
+
+#you can mix qoutes
+
+puts "Helllo 'you' to "
