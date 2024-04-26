ae2d960842abdacedcf1a265e1b8723f8e5788dd
IsCathal <mccabemcathal@gmail.com>
2024-04-26 08:35:31 +0100

    basic maths

---
+++ basic_arithmetic_in_ruby.rb
@@ -0,0 +1,22 @@
+puts 1 + 5
+
+puts 3*4
+
+#followws PEMDAS basic maths remember you did honours maths
+
+puts (3*8) + 5
+
+#ruby chops off additional to a division example 2.9 will be 2 etc
+
+puts 12/5
+
+
+#use floats decimal symbol for exactish answer
+
+puts 12.0/5.0
+
+# % is the modulaus give the remainer, useful for commin problem solving issues
+
+puts 5%2
+
+puts 6%2
