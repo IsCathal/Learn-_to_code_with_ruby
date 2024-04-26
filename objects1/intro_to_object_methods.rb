ac0ee100418e388400580b9bb5af32c74dd94744
IsCathal <mccabemcathal@gmail.com>
2024-04-26 15:08:51 +0100

    intro

---
+++ objects1/intro_to_object_methods.rb
@@ -0,0 +1,13 @@
+#strings are objects in ruby
+
+puts "helloWorld".length
+
+puts "helloWorld".upcase
+
+puts "ALLCAPSTODOWN".downcase
+
+expression = " ALLCAPS"
+
+puts expression.downcase
+
+#can also add downcase() parentisiss
