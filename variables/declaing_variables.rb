dfa65cffa55745d91d474d43f55662f7b2f5b5af
IsCathal <mccabemcathal@gmail.com>
2024-04-26 10:57:13 +0100

    var

---
+++ variables/declaing_variables.rb
@@ -0,0 +1,25 @@
+age = 31
+puts age
+
+name = 'Boris'
+
+last_name = "Danger"
+
+
+puts name
+puts age + 7
+
+puts name + "  " + last_name
+
+#read right from left, right habd side evaluted first
+age = age + 7
+
+puts age
+
+#cant really do this in java, maybe javascript
+chameleon = 24
+chameleon = "asbkgjrefj"
+chameleon = 3.12
+
+puts chameleon
+
