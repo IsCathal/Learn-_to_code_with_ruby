5194268d303eaf1ed8416b51084084af3ec2e16d
IsCathal <mccabemcathal@gmail.com>
2024-04-26 17:19:53 +0100

    nil object

---
+++ objects1/nil_object.rb
@@ -0,0 +1,13 @@
+puts nil
+p nil
+#no value
+
+#every method in ruby returns a value
+
+#example
+
+what_is_the_value_here = puts "Hello"
+
+p what_is_the_value_here
+
+#returns null has nothing to give you so returns null
---
+++ objects1/the_inspect_method.rb
@@ -0,0 +1,9 @@
+p "Hello World"
+
+#.inspect method will give you a better view on your objects
+#p is calling that method
+
+
+puts "Hello world\n".inspect
+
+p "Helloworld\n"
