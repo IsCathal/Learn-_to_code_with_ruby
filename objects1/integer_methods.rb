764bfd639812241a2a4048bebe5529c8b6a53af1
IsCathal <mccabemcathal@gmail.com>
2024-04-26 16:54:01 +0100

    mehtodchaining

---
+++ objects1/integer_methods.rb
@@ -0,0 +1,10 @@
+#everything in ruby is an object
+
+puts 10.next
+
+puts -1.next
+
+puts 10.succ
+
+puts 10.pred
+
---
+++ objects1/method_changing.rb
@@ -0,0 +1,4 @@
+puts "hi there".upcase.length.succ
+#can chain methods
+
+puts 10.next.next.next().next
