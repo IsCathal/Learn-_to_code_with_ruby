4ed09f7dd61eae348226f6a6f8751e3b8f701f91
IsCathal <mccabemcathal@gmail.com>
2024-05-01 17:55:18 +0100

    some arrays

---
+++ array/access_and_overwrite_multiple_array_elements.rb
@@ -0,0 +1,30 @@
+sesame_street = [ 
+    "Elmo",
+    "Big Bird",
+    "Cookie Monster",
+    "Bert",
+    "Ernie",
+    "Oscar"
+
+]
+
+
+#start at and do how mant
+p sesame_street[0, 02]
+#as most as it can if not 10 elements
+p sesame_street[0, 10]
+
+
+p sesame_street.slice(0,3)
+
+
+
+puts
+#find forth element takes 2 and replaced them 
+p sesame_street[3,2] = ["Skiny", "Kermit"]
+
+
+
+p sesame_street[3,2] = ["Skiny", "Kermit", "kermit"]
+
+p sesame_street
