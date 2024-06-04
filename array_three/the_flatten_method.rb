3afd44a6eb0697ded6e3a4685014d93ec86f5937
IsCathal <mccabemcathal@gmail.com>
2024-06-04 14:32:06 +0100

    Clean up

---
+++ array_three/the_flatten_method.rb
@@ -0,0 +1,10 @@
+# flatten method - create a 1-dimensioanl array ( no nested arrays)
+
+party_attendee = [
+    "Joe",
+    ["bob", "keith", "Cam"],
+    "Holly",
+    ["Rick", "Molly"]
+]
+
+p party_attendee.flatten
---
+++ array_three/the_sample_method.rb
@@ -0,0 +1,5 @@
+# sample method - extract one or more random elements from an array
+
+flavors = ["Chocolate", "Vanilla", "Stawberry", "Cookies and Cream"]
+
+p flavors.sample
